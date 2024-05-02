24e4e63703f7ad20f984c17a01d14b389342b290
H4T_K1D <noahthecube1234@gmail.com>
2024-05-02 11:31:53 +0200

    Update BreakATake.java

--- src/main/java/com/h4t_k1d/breakatake/BreakATake.java
+++ src/main/java/com/h4t_k1d/breakatake/BreakATake.java
@@ -2,13 +2,11 @@ package com.h4t_k1d.breakatake;
 
 import com.h4t_k1d.breakatake.client.GUI;
 import com.h4t_k1d.breakatake.client.TickHandler;
-import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
-import org.slf4j.Logger;
 
 @Mod(BreakATake.MODID)
 public class BreakATake
@@ -17,9 +15,6 @@ public class BreakATake
     private static final TickHandler tickHandler = new TickHandler();
     // Set modid
     public static final String MODID = "breakatake";
-    // Get current logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-
 
     public BreakATake(IEventBus modEventBus, ModContainer modContainer)
     {
